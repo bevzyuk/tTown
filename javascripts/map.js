978572c21eb759c00f59de08e486df0424338f07
bevzyuk <oleksandr.bevzyuk@gmail.com>
2016-05-17 15:44:42 +0300

    change style to map

--- javascripts/map.js
+++ javascripts/map.js
@@ -37,16 +37,36 @@ function loadMap() {
 
             map.mapTypes.set("map_style", styledMap);
             map.setMapTypeId("map_style");
+
             var marker = new google.maps.Marker({
                position: new google.maps.LatLng(50.4102576,30.6156543),
                map: map,
             });
 
+            var firstMeet = new google.maps.Marker({
+               position: new google.maps.LatLng(50.4487674,30.5118484),
+               map: map,
+            });
+
+            var coffeRainig = new google.maps.Marker({
+               position: new google.maps.LatLng(50.462513,30.4800016),
+               map: map,
+            });
+
+            var infocoffe = new google.maps.InfoWindow({
+               content: "Тут ми пили каву під зонтом",
+            })
+
+             var infowin = new google.maps.InfoWindow({
+               content: "Place were we're first met :)",
+            })
+
             var infowindow = new google.maps.InfoWindow({
                content: "We are living here!!",
             });
 
-           
+           infowin.open(map,firstMeet);
+           infocoffe.open(map,coffeRainig);
 
            //Zoom to 7 when clicked on marker
             google.maps.event.addListener(marker,'click',function() {
