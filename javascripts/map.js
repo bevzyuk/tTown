5f8ff546c149173ded621ee7a39e683047597533
bevzyuk <oleksandr.bevzyuk@gmail.com>
2016-05-17 15:30:00 +0300

    add style to map

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,13 +1,21 @@
+var styles = [ { "featureType": "water", "elementType": "geometry", "stylers": [ { "visibility": "simplified" }, { "hue": "#007fff" }, { "saturation": 1 }, { "color": "#007fff" }, { "lightness": 65 } ] },{ "featureType": "landscape", "stylers": [ { "color": "#008080" } ] },{ "featureType": "road.local", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road", "stylers": [ { "hue": "#0055ff" }, { "color": "#0055ff" }, { "visibility": "on" }, { "lightness": 43 } ] },{ "featureType": "landscape.natural", "stylers": [ { "color": "#00ffaa" }, { "lightness": -50 } ] },{ } ];
+
+var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
 function loadMap() {
 			
             var mapOptions = {
                center:new google.maps.LatLng(50.45,30.523333),
                zoom:13,
-               mapTypeId:google.maps.MapTypeId.ROADMAP
+               mapTypeControlOption: {
+               mapTypeId:[google.maps.MapTypeId.ROADMAP,'map_style']
+                    }
             };
 				
             var map = new google.maps.Map(document.getElementById("sample"),mapOptions);
             
+
+            map.mapTypes.set("map_style", styledMap);
+            map.setMapTypeId("map_style");
             var marker = new google.maps.Marker({
                position: new google.maps.LatLng(50.4102576,30.6156543),
                map: map,
